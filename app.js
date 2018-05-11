b277b4015be0a7bdbed6452f8e2fc686b133a1ff
Yi Kai <beliefs111@gmail.com>
2018-05-11 18:19:59 +0800

    remove result page

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 //app.js
+const updateManager = wx.getUpdateManager();
+
 App({
   onLaunch: function () {
     console.log('app lauched')
+    updateManager.onUpdateReady(function () {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+
+    })
   },
   globalData: {}
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,10 @@ function getRandomInt(min, max) {
 Page({
   data: {
     logo: '../../images/logo.png',
-    lists: [{}, {}]
+    lists: [{}, {}],
+    optionCount: 0,
+    curView: 0,
+    result: ''
   },
   addList: function () {
     var lists = this.data.lists
@@ -16,42 +19,72 @@ Page({
   },
   delList: function () {
     var lists = this.data.lists
-    lists.pop()  
+    lists.pop()
+    var optionCount = 0
+    Object.keys(lists).forEach(key => {
+      if (lists[key]['value']) {
+        optionCount += 1
+      }
+    })
     this.setData({
       lists: lists,
+      optionCount: optionCount
     })
   },
   bindKeyInput: function(e) {
     var lists = this.data.lists
     lists[e.target.dataset.id]['value'] = e.detail.value
+    var optionCount = 0
+    Object.keys(lists).forEach(key => {
+      if (lists[key]['value']) {
+        optionCount += 1
+      }
+    })
     this.setData({
-      lists: lists
+      lists: lists,
+      optionCount: optionCount
     })
   },
   formSubmit: function (e) {
     var options = e.detail.value
-    var indices = Object.keys(options)
-    var minIndex = Math.min(...indices)
-    var maxIndex = Math.max(...indices)
-    var theIndex = getRandomInt(minIndex, maxIndex)
-    var result = options[theIndex]
-    if (result === ''){
+    Object.keys(options).forEach((key) => {
+      if (!options[key]) delete options[key]
+    })
+    if (Object.keys(options).length === 0) {
       wx.showToast({
         title: '好像没有选项哦',
         image: this.data.logo
       })
       return
-    }
-    wx.showLoading({
-      title: '决策姬正在思考',
-      mask: true
-    })
-    setTimeout(() => {
-      wx.hideLoading()
-      wx.navigateTo({
-        url: '../result/result?result=' + result
+    } else if (Object.keys(options).length === 1) {
+      wx.showToast({
+        title: '只有一个选项呢',
+        image: this.data.logo
       })
-    }, 1500)
+      return
+    }
+    var indices = Object.keys(options)
+    var minIndex = Math.min(...indices)
+    var maxIndex = Math.max(...indices)
+    var theIndex = getRandomInt(minIndex, maxIndex)
+    var result = options[theIndex]
+    this.setData({
+      curView: 1,
+      result: result,
+    })
+    // wx.showLoading({
+    //   title: '决策姬正在思考',
+    //   mask: true
+    // })
+    setTimeout(() => {
+      this.setData({
+        curView: 2,
+      })
+      // wx.hideLoading()
+      // wx.navigateTo({
+      //   url: '../result/result?result=' + result
+      // })
+    }, 2000)
   },
   formReset: function(e) {
     var lists = this.data.lists
@@ -59,7 +92,15 @@ Page({
     for (let i = 0; i < lists.length; i++) {
       newLists.push({})
     }
-    this.setData({lists: newLists})
+    this.setData({
+      lists: newLists,
+      optionCount: 0
+    })
+  },
+  backToMain: function () {
+    this.setData({
+      curView: 0
+    })
   },
   onShareAppMessage: function () {
     return {
